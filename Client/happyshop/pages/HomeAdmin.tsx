86e6174e5ada557f5933078722686314df910fcf
karimabdelehi <karimabdelhedi080@gmail.com>
2022-12-19 22:05:02 +0100

    Admin design done

--- Client/happyshop/pages/HomeAdmin.tsx
+++ Client/happyshop/pages/HomeAdmin.tsx
@@ -1,20 +1,31 @@
 import Link from "next/link";
-import React from "react";
+import React from "react"; 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 export default function HomeAdmin() {
   return (
-    <div>
-     <li className="active">
-                <Link href='/AllProductAdmin' >AllProductAdmin</Link>
-              </li>
-              <li className="active">
-                <Link href='/jeansadmin' >jeansadmin</Link>
-              </li>
-              <li className="active">
-                <Link href='/shoesadmin' >shoesadmin</Link>
-              </li>
-              <li className="active">
-                <Link href='/T-SHIRTadmin' >T-SHIRTadmin</Link>
-              </li>
+    <div> 
+      
+     <button className="active8">
+                <Link href='/AllProductAdmin' >WELCOME ADMIN</Link>
+              </button>
+              <div>
+              <Carousel>
+                
+                  <div>
+                      <img src="https://res.cloudinary.com/dqmhtibfm/image/upload/v1671285641/happyshop/carousel-1_mt7jow.jpg" alt="image1"/>
+  
+                  </div>
+                  <div>
+                      <img src="https://res.cloudinary.com/dqmhtibfm/image/upload/v1671285719/happyshop/carousel-3_po3lyg.jpg" alt="image2" />
+  
+                  </div>
+                  <div>
+                      <img src="https://res.cloudinary.com/dqmhtibfm/image/upload/v1671286702/happyshop/carousel-2_qmoivl.jpg" alt="image3"/>
+                  </div>
+
+              </Carousel>
+            </div>
     </div>
   );
 }
